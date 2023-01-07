8e759df5c5c4410dc1d278f9628422c29e2816a0
jonniek01 <jontejonhn@gmail.com>
2023-01-07 08:39:03 +0300

    set up complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import video from './back.mp4'
+import logo from './logo-transparent.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay loop muted id='video' >
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="content">
+        <img src={logo} alt="PPEER" />
+        <h3>We enable seamless staking in peer to peer gaming</h3>
+        </div>
+        <div className="buttons">
+          <a href='mailto:johnndigirigi01@gmail.com'>CONTACT US</a>
+          <a onClick={()=>{alert("APP UNDER DEVELOPMENT")}} href='#video'>DOWNLOAD APP</a>
+        </div>
     </div>
   );
 }
